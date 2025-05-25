59ae45647b5245076188920d35127937194e4485
Kata Venkata Sai Krishna <venkata.krishna@mediamelon.com>
2025-05-25 19:34:50 +0530

    initial commit with QoE SDK for SPM

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.6
+import PackageDescription
+
+let package = Package(
+  name: "MediaMelonQoE",
+  platforms: [
+    .iOS(.v12),
+    .tvOS(.v12)
+  ],
+  products: [
+    .library(
+      name: "MediaMelonQoE",
+      targets: ["MediaMelonQoE"]
+    )
+  ],
+  targets: [
+    .binaryTarget(
+      name: "MediaMelonQoE",
+      path: "./MediaMelonQoE.xcframework"
+    )
+  ]
+)
